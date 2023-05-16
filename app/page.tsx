a6e0b7b1c97663df0ffe40c18ff0febe118e5755
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2023-05-16 16:20:05 +0000

    date

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,9 @@ export default function Home() {
     if (!data) return;
 
     return data.reduce((acc: any, val: any) => {
-      const date = new Date(val.played_at).toLocaleString("en-US")?.match(/\d{2}\/\d{2}\/\d{4}/g).toString();
+      const date = new Date(val.played_at).toLocaleString("en-US").toString().match(/\d{4}-\d{2}-\d{2}/g)?.toString();
+      if(!date) return;
+
       const item = acc.find((item: any) =>
         item.date.match(new RegExp(date, "g"))
       );
