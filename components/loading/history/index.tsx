36930431954da8552da3212abb854e625edf8261
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2023-06-02 16:33:45 +0000

    Fix styling again

--- components/loading/history/index.tsx
+++ components/loading/history/index.tsx
@@ -11,7 +11,7 @@ export function HistoryLoading() {
         <div key={index}>
             <h3 className="flex items-center pb-4 text-base font-bold text-slate-400">
                 <LucideCalendarClock size={16} className="mr-2" />
-                <Skeleton className="h-4 w-[150px] max-w-full" />
+                <Skeleton className="h-6 w-[150px] max-w-full" />
             </h3>
             <ul className="ml-1.5 flex flex-col gap-2 border-l pl-4">
                 {[...new Array(5)].map((val: any, index: number) => (
