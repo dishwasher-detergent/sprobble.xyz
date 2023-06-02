810e31d580e2d44d5591e996242f05fbb69b1878
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2023-06-02 16:37:05 +0000

    revert style

--- components/loading/history/item.tsx
+++ components/loading/history/item.tsx
@@ -6,13 +6,13 @@ export default function HistoryItemLoading() {
       <div className="relative flex flex-row items-start gap-2 rounded-lg p-1">
         <Skeleton className="h-24 w-24 rounded-lg" />
         <div className="h-24 flex flex-col gap-1">
-          <Skeleton className="h-2 w-[200px] max-w-full" />
+          <Skeleton className="h-4 w-[200px] max-w-full" />
           <Skeleton className="h-6 w-[250px] max-w-full" />
-          <Skeleton className="h-2 w-[150px] max-w-full" />
+          <Skeleton className="h-4 w-[150px] max-w-full" />
             <div className="flex flex-row gap-1">
-                <Skeleton className="h-2 w-[75px]" />
-                <Skeleton className="h-2 w-[75px]" />
-                <Skeleton className="h-2 w-[75px]" />
+                <Skeleton className="h-4 w-[75px]" />
+                <Skeleton className="h-4 w-[75px]" />
+                <Skeleton className="h-4 w-[75px]" />
             </div>
         </div>
       </div>
