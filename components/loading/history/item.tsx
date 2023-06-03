0e401d5b436359f45730c4fe81406fc4d8bbef01
Kenneth Bass <kenny_bass@outlook.com>
2023-06-03 17:07:51 -0500

    fix skeleton?

--- components/loading/history/item.tsx
+++ components/loading/history/item.tsx
@@ -5,21 +5,21 @@ export default function HistoryItemLoading() {
     <article>
       <div className="relative flex flex-row items-start gap-2 rounded-lg p-1">
         <Skeleton className="h-24 w-24 rounded-lg" />
-        <div className="h-24 flex flex-col gap-1">
-          <Skeleton className="h-4 w-[200px] max-w-full" />
-          <Skeleton className="h-6 w-[250px] max-w-full" />
-          <Skeleton className="h-4 w-[150px] max-w-full" />
-            <div className="flex flex-row gap-1">
-                <Skeleton className="h-4 w-[75px]" />
-                <Skeleton className="h-4 w-[75px]" />
-                <Skeleton className="h-4 w-[75px]" />
-            </div>
+        <div className="flex h-24 flex-col gap-1">
+          <Skeleton className="h-4 w-full max-w-[200px]" />
+          <Skeleton className="h-6 w-full max-w-[250px]" />
+          <Skeleton className="h-4 w-full max-w-[150px]" />
+          <div className="flex flex-row gap-1">
+            <Skeleton className="h-4 w-[75px]" />
+            <Skeleton className="h-4 w-[75px]" />
+            <Skeleton className="h-4 w-[75px]" />
+          </div>
         </div>
       </div>
-        <div className="text-slate-300 flex items-center text-sm">
-          Listened by&nbsp;
-          <Skeleton className="h-4 w-[150px]" />
-        </div>
+      <div className="flex items-center text-sm text-slate-300">
+        Listened by&nbsp;
+        <Skeleton className="h-4 w-full max-w-[150px]" />
+      </div>
     </article>
   );
 }
