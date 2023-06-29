b72cb7bfbab6cf4e7d677e2d4131c515be1dee20
Kenneth Bass <kenny_bass@outlook.com>
2023-06-28 21:11:14 -0500

    Fix v1 url

--- app/global/stats/album/page.tsx
+++ app/global/stats/album/page.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 
 async function getTop() {
   const album: Models.Document & Album = await fetch(
-    `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/v1/databases/645c032960cb9f95212b/collections/album/documents?queries[0]=limit(1)&queries[1]=orderDesc($createdAt)`,
+    `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/databases/645c032960cb9f95212b/collections/album/documents?queries[0]=limit(1)&queries[1]=orderDesc($createdAt)`,
     {
       next: {
         revalidate: 60,
