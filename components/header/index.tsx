d7b66392ddce5ceafa23ad1c7e128cdfc261a71f
Kenneth Bass <kenny_bass@outlook.com>
2023-07-30 16:27:35 -0500

    fix header with no image

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -29,36 +29,47 @@ export function Header({
 
   return (
     <div className={cn("relative", className)} {...props}>
-      <div
-        className="h-36 w-full rounded-lg md:h-52"
-        style={{
-          background:
-            data &&
-            `linear-gradient(45deg, ${getHSL(data[0], 1)} 0%, ${getHSL(
-              data[1],
-              1
-            )} 100%)`,
-        }}
-      />
-      <div className="-mt-24 ml-8 md:-mt-36 md:ml-16">
-        {artwork && artwork_name && (
-          <div className="relative aspect-square h-32 w-32 overflow-hidden rounded-lg bg-slate-300 ring-4 ring-background md:h-60 md:w-60">
-            <img
-              src={artwork}
-              alt={artwork_name}
-              sizes="(max-width: 256px) 100vw"
-            />
+      {artwork ? (
+        <>
+          <div
+            className="h-36 w-full rounded-lg md:h-52"
+            style={{
+              background:
+                data &&
+                `linear-gradient(45deg, ${getHSL(data[0], 1)} 0%, ${getHSL(
+                  data[1],
+                  1
+                )} 100%)`,
+            }}
+          />
+          <div className="-mt-24 ml-8 md:-mt-36 md:ml-16">
+            <div className="relative aspect-square h-32 w-32 overflow-hidden rounded-lg bg-slate-300 ring-4 ring-background md:h-60 md:w-60">
+              <img
+                src={artwork}
+                alt={artwork_name}
+                sizes="(max-width: 256px) 100vw"
+              />
+            </div>
+            <div className="mt-2 flex-1">
+              <h2 className="text-xl font-black dark:text-white md:text-3xl">
+                {title}
+              </h2>
+              <p>{subTitle}</p>
+              <div>{description}</div>
+              <p>{listen}</p>
+            </div>
           </div>
-        )}
+        </>
+      ) : (
         <div className="flex-1">
-          <h2 className="text-xl font-black dark:text-white md:text-3xl">
+          <h2 className="text-3xl font-black dark:text-white md:text-6xl">
             {title}
           </h2>
           <p>{subTitle}</p>
           <div>{description}</div>
           <p>{listen}</p>
         </div>
-      </div>
+      )}
     </div>
   );
 }
