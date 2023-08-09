7f57b7722c4b22fefa18ccf41fda248a0c645521
Kenneth Bass <kenny_bass@outlook.com>
2023-08-08 19:37:40 -0500

    add metadata

--- app/global/leaderboard/page.tsx
+++ app/global/leaderboard/page.tsx
@@ -1,6 +1,11 @@
 import { Header } from "@/components/header";
 import { Leaderboard } from "@/components/leaderboard/leaderboard";
 
+export const metadata = {
+  title: "Sprobble Leaderboard",
+  description: "Leaderboard for all users on Sprobble.xyz",
+};
+
 export default function LeaderboardPage() {
   return (
     <section className="space-y-6">
