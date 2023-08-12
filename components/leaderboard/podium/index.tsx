0eb6406cd176a088e30dcc3fa04e99b1a12026ef
Kenneth Bass <kenny_bass@outlook.com>
2023-08-11 20:28:20 -0500

    update podium style

--- components/leaderboard/podium/index.tsx
+++ components/leaderboard/podium/index.tsx
@@ -7,15 +7,24 @@ interface PodiumProps {
 
 export default function Podium({ data }: PodiumProps) {
   return (
-    <section className="flex flex-col gap-4 md:flex-row">
-      {data.map((item: any, index: number) => (
-        <PodiumItem
-          key={index}
-          image={`https://data.kennethbass.com/v1/storage/buckets/${avatarBucketId}/files/${item.id}/preview?project=${projectId}`}
-          title={item.name}
-          position={index}
-        />
-      ))}
+    <section className="flex flex-col items-end justify-center gap-4 md:flex-row">
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, fit-content(1fr))",
+          alignItems: "end",
+          gap: "1rem",
+        }}
+      >
+        {data.map((item: any, index: number) => (
+          <PodiumItem
+            key={index}
+            image={`https://data.kennethbass.com/v1/storage/buckets/${avatarBucketId}/files/${item.id}/preview?project=${projectId}`}
+            title={item.name}
+            position={index}
+          />
+        ))}
+      </div>
     </section>
   );
 }
