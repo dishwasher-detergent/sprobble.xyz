c6d28dcbccadae34595024a3a9cd79ffe270ab0f
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2023-08-16 09:20:56 -0500

    Update page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -44,7 +44,7 @@ export default function AccountPage() {
 
       if (!existingUser) {
         const image = await fetchAndSaveImage(
-          `https://api.dicebear.com/6.x/thumbs/png?seed=${account.$id}`,
+          `https://api.dicebear.com/6.x/thumbs/png?seed=${account.$id}&backgroundColor=fca5a5,fdba74,fcd34d,fde047,bef264,86efac,6ee7b7,5eead4,67e8f9,7dd3fc,93c5fd,a5b4fc,c4b5fd,d8b4fe,f0abfc,f9a8d4,fda4af&shapeColor=dc2626,ea580c,d97706,ca8a04,65a30d,16a34a,059669,0d9488,0891b2,0284c7,2563eb,4f46e5,7c3aed,9333ea,c026d3,db2777,e11d48`,
           `${account.$id}.svg`
         );
 
