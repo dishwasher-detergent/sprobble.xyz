dd5ca777c06689899f08ff0eac4e75dcba153d76
Kenneth Bass <kenny_bass@outlook.com>
2024-02-08 17:04:10 -0600

    start fresh

--- components/sidebar/index.tsx
+++
@@ -1,9 +0,0 @@
-import { SidebarContent } from "@/components/sidebar/content";
-
-export function Sidebar() {
-  return (
-    <aside className="hidden h-full w-64 flex-none flex-col overflow-y-auto border-r bg-background px-4 py-8 shadow-sm md:flex">
-      <SidebarContent />
-    </aside>
-  );
-}
--- functions/stats/src/utils/appwrite.ts
+++
@@ -1,8 +0,0 @@
-export const databaseId = "645c032960cb9f95212b";
-export const trackCollectionId = "track";
-export const artistCollectionId = "artist";
-export const albumCollectionId = "album";
-export const userCollectionId = "user";
-export const statsCollectionId = "stats";
-export const playsCollectionId = "plays";
-export const totalStatsCollectionId = "total_stats";
