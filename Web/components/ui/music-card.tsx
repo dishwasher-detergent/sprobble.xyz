6e1ce4127004eab2242a976e4485bcf6a706a5d9
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2024-02-09 14:57:10 -0600

    fix

--- Web/components/ui/music-card.tsx
+++ Web/components/ui/music-card.tsx
@@ -64,7 +64,7 @@ export function MusicCard({
       <div className="w-full flex-row overflow-hidden rounded-full bg-slate-100 p-1 text-slate-800">
         <div className="flex flex-row items-center text-xs md:text-sm">
           <div className="flex flex-none flex-row flex-nowrap items-center gap-2">
-            {user.avatar && (
+            {user?.avatar && (
               <div className="aspect-square h-6 w-6 flex-none overflow-hidden rounded-full md:h-8 md:w-8">
                 <img
                   className="h-full w-full object-cover object-center"
