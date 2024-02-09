c65e3847bf4ec41af2dd131040d802171dc75990
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2024-02-09 10:39:46 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  exclude: ['functions'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+        port: "",
+      }
+    ],
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
