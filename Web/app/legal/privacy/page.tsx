80bd7a0797c64ddec9c8a352709893ed88c6a6ff
Kenneth Bass <kenny_bass@outlook.com>
2024-02-17 19:50:14 -0600

    fix quotes lint

--- Web/app/legal/privacy/page.tsx
+++ Web/app/legal/privacy/page.tsx
@@ -43,15 +43,16 @@ export default function Privacy() {
           <p>
             <strong>Affiliate</strong> means an entity that controls, is
             controlled by or is under common control with a party, where
-            "control" means ownership of 50% or more of the shares, equity
-            interest or other securities entitled to vote for election of
+            &quot;control&quot; means ownership of 50% or more of the shares,
+            equity interest or other securities entitled to vote for election of
             directors or other managing authority.
           </p>
         </li>
         <li>
           <p>
-            <strong>Company</strong> (referred to as either "the Company", "We",
-            "Us" or "Our" in this Agreement) refers to Sprobble.xyz.
+            <strong>Company</strong> (referred to as either &quot;the
+            Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in
+            this Agreement) refers to Sprobble.xyz.
           </p>
         </li>
         <li>
@@ -151,7 +152,7 @@ export default function Privacy() {
       <h4>Usage Data</h4>
       <p>Usage Data is collected automatically when using the Service.</p>
       <p>
-        Usage Data may include information such as Your Device's Internet
+        Usage Data may include information such as Your Device&apos;s Internet
         Protocol address (e.g. IP address), browser type, browser version, the
         pages of our Service that You visit, the time and date of Your visit,
         the time spent on those pages, unique device identifiers and other
@@ -181,14 +182,14 @@ export default function Privacy() {
       <p>
         If You decide to register through or otherwise grant us access to a
         Third-Party Social Media Service, We may collect Personal data that is
-        already associated with Your Third-Party Social Media Service's account,
-        such as Your name, Your email address, Your activities or Your contact
-        list associated with that account.
+        already associated with Your Third-Party Social Media Service&apos;s
+        account, such as Your name, Your email address, Your activities or Your
+        contact list associated with that account.
       </p>
       <p>
         You may also have the option of sharing additional information with the
-        Company through Your Third-Party Social Media Service's account. If You
-        choose to provide such information and Personal Data, during
+        Company through Your Third-Party Social Media Service&apos;s account. If
+        You choose to provide such information and Personal Data, during
         registration or otherwise, You are giving the Company permission to use,
         share, and store it in a manner consistent with this Privacy Policy.
       </p>
@@ -219,10 +220,10 @@ export default function Privacy() {
         </li>
       </ul>
       <p>
-        Cookies can be "Persistent" or "Session" Cookies. Persistent Cookies
-        remain on Your personal computer or mobile device when You go offline,
-        while Session Cookies are deleted as soon as You close Your web browser.
-        You can learn more about cookies on{" "}
+        Cookies can be &quot;Persistent&quot; or &quot;Session&quot; Cookies.
+        Persistent Cookies remain on Your personal computer or mobile device
+        when You go offline, while Session Cookies are deleted as soon as You
+        close Your web browser. You can learn more about cookies on{" "}
         <a
           href="https://www.termsfeed.com/blog/cookies/#What_Are_Cookies"
           target="_blank"
@@ -311,10 +312,11 @@ export default function Privacy() {
           <p>
             <strong>To contact You:</strong> To contact You by email, telephone
             calls, SMS, or other equivalent forms of electronic communication,
-            such as a mobile application's push notifications regarding updates
-            or informative communications related to the functionalities,
-            products or contracted services, including the security updates,
-            when necessary or reasonable for their implementation.
+            such as a mobile application&apos;s push notifications regarding
+            updates or informative communications related to the
+            functionalities, products or contracted services, including the
+            security updates, when necessary or reasonable for their
+            implementation.
           </p>
         </li>
         <li>
@@ -412,12 +414,13 @@ export default function Privacy() {
       </p>
       <h3>Transfer of Your Personal Data</h3>
       <p>
-        Your information, including Personal Data, is processed at the Company's
-        operating offices and in any other places where the parties involved in
-        the processing are located. It means that this information may be
-        transferred to — and maintained on — computers located outside of Your
-        state, province, country or other governmental jurisdiction where the
-        data protection laws may differ than those from Your jurisdiction.
+        Your information, including Personal Data, is processed at the
+        Company&apos;s operating offices and in any other places where the
+        parties involved in the processing are located. It means that this
+        information may be transferred to — and maintained on — computers
+        located outside of Your state, province, country or other governmental
+        jurisdiction where the data protection laws may differ than those from
+        Your jurisdiction.
       </p>
       <p>
         Your consent to this Privacy Policy followed by Your submission of such
@@ -489,7 +492,7 @@ export default function Privacy() {
         means to protect Your Personal Data, We cannot guarantee its absolute
         security.
       </p>
-      <h2>Children's Privacy</h2>
+      <h2>Children&apos;s Privacy</h2>
       <p>
         Our Service does not address anyone under the age of 13. We do not
         knowingly collect personally identifiable information from anyone under
@@ -502,15 +505,15 @@ export default function Privacy() {
       <p>
         If We need to rely on consent as a legal basis for processing Your
         information and Your country requires consent from a parent, We may
-        require Your parent's consent before We collect and use that
+        require Your parent&apos;s consent before We collect and use that
         information.
       </p>
       <h2>Links to Other Websites</h2>
       <p>
         Our Service may contain links to other websites that are not operated by
         Us. If You click on a third party link, You will be directed to that
-        third party's site. We strongly advise You to review the Privacy Policy
-        of every site You visit.
+        third party&apos;s site. We strongly advise You to review the Privacy
+        Policy of every site You visit.
       </p>
       <p>
         We have no control over and assume no responsibility for the content,
@@ -523,8 +526,8 @@ export default function Privacy() {
       </p>
       <p>
         We will let You know via email and/or a prominent notice on Our Service,
-        prior to the change becoming effective and update the "Last updated"
-        date at the top of this Privacy Policy.
+        prior to the change becoming effective and update the &quot;Last
+        updated&quot; date at the top of this Privacy Policy.
       </p>
       <p>
         You are advised to review this Privacy Policy periodically for any
