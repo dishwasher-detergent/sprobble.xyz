84f46df121cb6e127271d6a1ec9012512a396bdf
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2024-02-20 14:52:54 -0600

    Style test

--- Web/components/ui/stat-card-container.tsx
+++ Web/components/ui/stat-card-container.tsx
@@ -4,7 +4,7 @@ interface StatCardContainerProps {
 
 export function StatCardContainer({ children }: StatCardContainerProps) {
   return (
-    <section className="relative z-10 grid grid-cols-1 gap-4 pb-4 md:grid-cols-2 xl:-mt-24 xl:grid-cols-3 xl:pb-16">
+    <section className="relative z-10 grid grid-cols-1 gap-4 p-2 mb-4 md:grid-cols-2 xl:-mt-24 xl:grid-cols-3 xl:mb-16 bg-slate-900/30 dark:bg-slate-100/30">
       {children}
     </section>
   );
