359cd3289b1367ebc5f80426f5cd022425a1fc84
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2024-02-20 16:37:23 -0600

    Fix ugly style tests :(

--- Web/components/ui/history-container.tsx
+++ Web/components/ui/history-container.tsx
@@ -4,7 +4,7 @@ interface HistoryContainerProps {
 
 export function HistoryContainer({ children }: HistoryContainerProps) {
   return (
-    <div className="grid grid-cols-1 gap-4 rounded-3xl p-2 bg-slate-900/10 dark:bg-slate-100/10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div className="grid grid-cols-1 gap-4 rounded-3xl bg-primary-foreground p-2 dark:bg-slate-900 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {children}
     </div>
   );
