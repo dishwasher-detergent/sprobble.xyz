b43ef847cc2291d79b124b1f08f3648020d20091
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2025-02-17 13:02:57 -0600

    Update hero-login-button.tsx

--- Web/components/ui/hero-login-button.tsx
+++ Web/components/ui/hero-login-button.tsx
@@ -9,17 +9,19 @@ import { auth_service } from "@/lib/appwrite";
 export function HeroLoginButton() {
   const { data, loading } = useAccount(true);
 
-  return !data ? (
-    <Button
-      aria-label="Login in with Spotify"
-      size="lg"
-      className="relative z-10 flex flex-row gap-2"
-      onClick={() => auth_service.createSpotifySession()}
-      disabled={loading}
-    >
-      {loading && <Loader className="text-white" />}
-      <Spotify variant="white" />
-      Log In with Spotify
-    </Button>
-  ) : null;
+  // return !data ? (
+  //   <Button
+  //     aria-label="Login in with Spotify"
+  //     size="lg"
+  //     className="relative z-10 flex flex-row gap-2"
+  //     onClick={() => auth_service.createSpotifySession()}
+  //     disabled={loading}
+  //   >
+  //     {loading && <Loader className="text-white" />}
+  //     <Spotify variant="white" />
+  //     Log In with Spotify
+  //   </Button>
+  // ) : null;
+
+  return null;
 }
